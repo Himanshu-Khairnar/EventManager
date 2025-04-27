8fb362e673369df062cfa77730ba433c91e032fc
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-04-27 22:18:42 +0530

    Created Folder Structure for page and element. Intialize routing

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,16 @@
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./page/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
-    <div>
-      <Button variant="ghost" >He;;p</Button>
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
