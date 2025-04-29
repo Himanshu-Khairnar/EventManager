94b038a6146f9eb64417cdd9f62aadf6101a78dc
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-04-29 16:15:24 +0530

    Completed Login and its testing

--- Server/src/controller/user.controllers.js
+++ Server/src/controller/user.controllers.js
@@ -33,7 +33,45 @@ export const signin = async (req, res) => {
 
   if (!updatedUser) throw new Error("error in adding refresh token");
 
-  const userData = await User.findById(id).select("-password -refreshToken");
+  const userData = await User.findById(id).select("-password ");
+  const Option = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .json({ userData, accessToken })
+    .cookie("accessToken", accessToken, Option)
+    .cookie("refreshToken", refreshToken, Option);
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    throw res.status(400).json({ error: "Email or Password in null" });
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ error: "Error in finding User" });
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect)
+    return res.status(400).json({ error: "Credentials are not Valid" });
+
+  const id = user._id
+  const fullName = user.fullName
+  const refreshToken = await generateToken(
+    { id, email, fullName },
+    "refreshToken"
+  );
+  const accessToken = await generateToken(
+    { id, email, fullName },
+    "accessToken"
+  );
+
+  const updatedUser = await User.findByIdAndUpdate(id, { refreshToken });
+
+  if (!updatedUser) throw new Error("error in adding refresh token");
+
+  const userData = await User.findById(id).select("-password  ");
   const Option = {
     httpOnly: true,
     secure: true,
--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -4,5 +4,7 @@ import asyncHandler from "express-async-handler";
 const router  = Router()
 
 import { signin } from "../controller/user.controllers.js";
+import { login } from "../controller/user.controllers.js";
 router.route("/signin").post(asyncHandler(signin));
+router.route("/login").post(asyncHandler(login));
 export default router;
