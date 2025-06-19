b001d09389793e7631ed011f76cc8cb9a1f58ff4
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-06-19 12:00:00 +0530

    Added routes in app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -14,12 +14,22 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+/// this for V1 of eventManagement
 //routes import
 import userRouter from "./routes/user.routes.js";
 import eventRouter from "./routes/event.routes.js";
+import revenueRouter from "./routes/revenue.routes.js";
+import registrationRouter from "./routes/registration.routes.js";
+import testimonialRouter from "./routes/testimonial.routes.js";
+import reviewRouter from "./routes/review.routes.js";
+const baseUrl = "/api/v1/";
 
 //routes declaration
-app.use("/users", userRouter);
-app.use("/", eventRouter);
+app.use(`${baseUrl}users`, userRouter);
+app.use(`${baseUrl}events`, eventRouter);
+app.use(`${baseUrl}revenue`, revenueRouter);
+app.use(`${baseUrl}registration`, registrationRouter);
+app.use(`${baseUrl}testimonial`, testimonialRouter);
+app.use(`${baseUrl}review`, reviewRouter);
 
 export { app };
