560acde7a9c8cc24cdc95b71abd715869679bad1
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-06-25 12:44:02 +0530

    added middleware of multer

--- Server/src/controller/user.controllers.js
+++ Server/src/controller/user.controllers.js
@@ -3,6 +3,15 @@ import { uploadFile } from "../Constant/Cloudinary";
 import User from "../models/user.model.js"
 export const signin =async (req, res) => {
     try {
+        //check imp data is null or not 
+        //check email 
+        // create tokens
+        // encrypt password
+        //save password 
+        //fetch data 
+        // send res
+
+        
         
     } catch (error) {
         
--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import asyncHandler from "express-async-handler";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 import { signin } from "../controller/user.controllers.js";
 import { login } from "../controller/user.controllers.js";
-router.route("/signin").post(asyncHandler(signin));
+router.route("/signin").upload.single("avatar").post(asyncHandler(signin));
 router.route("/login").post(asyncHandler(login));
 export default router;
